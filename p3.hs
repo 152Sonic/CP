c5c91463d32fc7bf996c1ac1a2c47df77335e8b1
homeless51 <carlosgba1@gmail.com>
2020-06-16 15:41:22 +0100

    Cenas alteradas

---
+++ p3.hs
@@ -0,0 +1,57 @@
+import Cp
+import List  hiding (fac)
+import Nat
+import BTree
+import LTree
+import Probability
+import ListUtils
+import Show
+import Data.List hiding (find)
+import Test.QuickCheck hiding ((><),choose,collect)
+import qualified Test.QuickCheck as QuickCheck
+import System.Random  hiding (split)
+import System.Process
+import GHC.IO.Exception
+import Graphics.Gloss
+import Control.Monad
+import Control.Applicative hiding ((<|>))
+import Exp
+
+
+
+
+tipsBdt :: Bdt a -> [a]
+tipsBdt = cataBdt (either singl ((uncurry (++)) . p2))
+tipsLTree = tips
+
+
+
+extLTree :: Bdt a -> LTree a
+extLTree = cataBdt g where
+  g = either Leaf (Fork . p2)
+
+
+
+inBdt = either Dec Bdt
+
+
+outBdt (Dec a) = Left a
+outBdt (Query a (t1,t2))  = Right(a,(t1,t2))
+
+
+baseBdt f g = id -|- (f >< (g >< g))
+
+
+recBdt f = baseBdt id f 
+
+
+
+cataBdt a = a . (recBdt (cataBdt a)) . outBdt
+
+
+
+anaBdt f = inBdt . ( recBdt (anaBdt f)) . f
+
+navLTree :: LTree a -> ([Bool] -> LTree a)
+navLTree = cataLTree g 
+  where g = undefined
